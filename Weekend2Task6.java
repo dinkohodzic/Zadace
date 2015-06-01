d5798d1ba62a602ffc14029f98a0b6cf66dc2b30
dinkohodzic <dinko.hodzic@bitcamp.ba>
2015-06-01 07:20:53 +0200

    Weekend 2 Task 6

---
+++ Weekend2Task6.java
@@ -0,0 +1,14 @@
+public class Weekend2Task6 {
+
+	public static void main(String[] args) {
+		Movie movie1 = new Movie("The Matrix", 1999, "The Wachowski Brothers",
+				"Sci-Fi", 8.6, true);
+		Movie movie2 = new Movie("Ex Machina", 2015, "Alex Garland",
+				"Sci-Fi, Drama ", 7.8, false);
+
+		System.out.println(movie1);
+		System.out.println(movie2);
+
+	}
+
+}
